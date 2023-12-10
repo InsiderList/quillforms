1f64f18cf5d34b46cd7d8a803ff9de61a8de5321
Rob McCombie <rob@tcadigital.com>
2023-12-10 12:15:50 +0000

    Update use-progress-percent.ts

--- packages/renderer-core/src/hooks/use-progress-percent.ts
+++ packages/renderer-core/src/hooks/use-progress-percent.ts
@@ -6,31 +6,31 @@ import useFlattenedBlocks from './use-flattened-blocks';
 import { FormBlocks } from '@quillforms/types/src';
 
 const useProgressPerecent = () => {
-	const { answers, walkPath, blockTypes } = useSelect( ( select ) => {
-		return {
-			walkPath: select( 'quillForms/renderer-core' ).getWalkPath(),
-			answers: select( 'quillForms/renderer-core' ).getAnswers(),
-			blockTypes: select( 'quillForms/blocks' ).getBlockTypes(),
-		};
-	} );
-	const allBlocks: FormBlocks = useFlattenedBlocks( walkPath );
-	let editableBlocksLength = 0;
-	let answered = 0;
-	allBlocks.forEach( ( field ) => {
-		if ( blockTypes[ field?.name ]?.supports.editable ) {
-			editableBlocksLength++;
-			if ( answers[ field?.id ]?.value ) {
-				answered++;
-			}
-		}
-	} );
-	const getPercent = () => {
-		const percent = Math.round( ( answered * 100 ) / editableBlocksLength );
-		if ( isNaN( percent ) ) return 0;
-		return percent;
-	};
+    const { answers, walkPath, blockTypes } = useSelect((select) => {
+        return {
+            walkPath: select('quillForms/renderer-core').getWalkPath(),
+            answers: select('quillForms/renderer-core').getAnswers(),
+            blockTypes: select('quillForms/blocks').getBlockTypes(),
+        };
+    });
+    const allBlocks: FormBlocks = useFlattenedBlocks(walkPath);
+    let totalBlocksLength = 0;
+    let answered = 0;
+    allBlocks.forEach((field) => {
+        totalBlocksLength++;
+        // If the block is editable and answered, or if it's a statement block, count it as answered
+        if ((blockTypes[field?.name]?.supports.editable && answers[field?.id]?.value) ||
+            field?.name === 'statement') { // Assuming 'statement' is the name for statement blocks
+            answered++;
+        }
+    });
+    const getPercent = () => {
+        const percent = Math.round((answered * 100) / totalBlocksLength);
+        if (isNaN(percent)) return 0;
+        return percent;
+    };
 
-	return getPercent();
+    return getPercent();
 };
 
 export default useProgressPerecent;
